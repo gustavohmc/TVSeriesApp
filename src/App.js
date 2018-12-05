f9df3b1ed21923d68e94e07854516167a8af4518
Gustavo Correa <gus.vancouver@gmail.com>
2018-12-04 21:58:19 -0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const Intro = (props) => (
+  <p className="App-intro">
+    Our first functional component.
+  </p>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            TV Series List
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <Intro />
       </div>
     );
   }
