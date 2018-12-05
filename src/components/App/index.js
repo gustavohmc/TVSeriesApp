3d4a5d0061fb072290f8538b98d2f3f9a9da5c62
Gustavo Correa <gus.vancouver@gmail.com>
2018-12-04 22:58:46 -0800

    compartiments

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,17 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Intro from '../intro';
 import 'whatwg-fetch';
+import Series from '../../containers/series'
 
 class App extends Component {
-  state = {
-    series: []
-  }
-
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-      .then(response => response.json())
-      .then(json => this.setState({series: json}))
-  }
 
   render() {
     return (
@@ -23,7 +15,8 @@ class App extends Component {
           </p>
         </header>
         <Intro message="Here you can find all of your most loved series"/>
-        The length of series array - {this.state.series.length}
+        
+        <Series />
       </div>
     );
   }
