fb1425efc580f6ddcb19e6ec86ac63b3d28b7a16
Gustavo Correa <gus.vancouver@gmail.com>
2018-12-10 20:49:50 -0800

    Updates

---
+++ precache-manifest.e6eb2987e82e613db017086aace6dbfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6a6686cbddb3e99a5f0b60a829effb3",
+    "url": "/TVSeriesApp/static/media/tenor.a6a6686c.gif"
+  },
+  {
+    "revision": "cf5aee76ea44abb0e89a",
+    "url": "/TVSeriesApp/static/js/runtime~main.cf5aee76.js"
+  },
+  {
+    "revision": "5e6511705c3cfba5c29b",
+    "url": "/TVSeriesApp/static/js/main.5e651170.chunk.js"
+  },
+  {
+    "revision": "fbdb3f117182f92dd0ea",
+    "url": "/TVSeriesApp/static/js/1.fbdb3f11.chunk.js"
+  },
+  {
+    "revision": "5e6511705c3cfba5c29b",
+    "url": "/TVSeriesApp/static/css/main.73b0cb8e.chunk.css"
+  },
+  {
+    "revision": "9f919da6ba1a744b1ab0279c10d70ce2",
+    "url": "/TVSeriesApp/index.html"
+  }
+];
